db0190159da828ce4c31ea338b1013796f21ab15
ElianaFuji <elianafuji@gmail.com>
2024-05-13 18:27:04 -0300

    Conta terminal com scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("Por favor digite o número de sua Agência: ");
+        String agenciaNumero = scanner.next();
+
+        System.out.println("Por favor digite o número de sua Conta Corrente: ");
+        int contaCorrente = scanner.nextInt();
+
+        System.out.println("Por favor digite seu Primeiro Nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor digite seu Sobrenome: ");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Por favor digite o valor do primeiro depósito: ");
+        double valorSaldo = scanner.nextDouble();
+  
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar um contrato em um banco, sua agência é " + agenciaNumero + ", com a conta número " + contaCorrente + " e seu saldo é " + valorSaldo);
+        
+        scanner.close();
+
     }
+    
 }
